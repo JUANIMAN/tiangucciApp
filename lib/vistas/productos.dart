b5e9eded90c01bb31a99dac33badb79e971e22d1
JUANIMAN <juanimandarin9@gmail.com>
2024-04-11 20:23:30 -0600

    Se agregaron mas productos de ejemplo

--- lib/vistas/productos.dart
+++ lib/vistas/productos.dart
@@ -1,4 +1,3 @@
-
 class Product {
   final String name;
   final double price;
@@ -15,29 +14,65 @@ class Product {
 
 final List<Product> productList = [
   Product(
-    name: 'Zapatillas Deportivas',
+    name: 'Tenis running',
     price: 1999.99,
-    image: Assets.imageZapatos,
-    description: 'Zapatillas cómodas para correr o hacer ejercicio.',
+    image: Assets.imagenZapatos,
+    description: 'Tenis cómodos para correr o hacer ejercicio.',
   ),
   Product(
     name: 'Bolso de Cuero',
     price: 2499.99,
-    image: Assets.imageBolso,
+    image: Assets.imagenBolso,
     description: 'Elegante bolso de cuero para ocasiones especiales.',
   ),
   Product(
       name: 'Reloj inteligente',
       price: 999.99,
-      image: Assets.imageReloj,
+      image: Assets.imagenReloj,
       description:
-      'Un reloj inteligente con seguimiento de actividad, notificaciones de mensajes y monitorización del ritmo cardíaco. Perfecto para mantenerse activo y conectado durante todo el día.')
+          'Un reloj inteligente con seguimiento de actividad, notificaciones de mensajes y monitorización del ritmo cardíaco. Perfecto para mantenerse activo y conectado durante todo el día.',
+  ),
+  Product(
+    name: 'Playera Deportiva',
+    price: 999.99,
+    image: Assets.imagenPlayera,
+    description: 'Camiseta ligera y transpirable para hacer ejercicio o practicar deportes.',
+  ),
+  Product(
+    name: 'Pantalones Deportivos',
+    price: 1299.99,
+    image: Assets.imagenPantalones,
+    description: 'Pantalones cómodos y ajustables para entrenamiento o actividades al aire libre.',
+  ),
+  Product(
+    name: 'Botella de Agua',
+    price: 499.99,
+    image: Assets.imagenBotella,
+    description: 'Botella de agua práctica y ecológica para llevar al gimnasio o a cualquier lugar.',
+  ),
+  Product(
+    name: 'Auriculares Inalámbricos',
+    price: 1499.99,
+    image: Assets.imagenAuriculares,
+    description: 'Auriculares inalámbricos con sonido de alta calidad para disfrutar de música mientras haces ejercicio.',
+  ),
+  Product(
+    name: 'Mancuernas Ajustables',
+    price: 1099.99,
+    image: Assets.imagenMancuernas,
+    description: 'Mancuernas ajustables para entrenamiento de fuerza en casa o en el gimnasio.',
+  ),
   // Agrega más productos aquí...
 ];
 
 mixin Assets {
   static const String imagePath = 'assets/images';
-  static const String imageZapatos = '$imagePath/zapatos.jpeg';
-  static const String imageBolso = '$imagePath/bolso.jpeg';
-  static const String imageReloj = '$imagePath/reloj.png';
+  static const String imagenZapatos = '$imagePath/zapatos.jpeg';
+  static const String imagenBolso = '$imagePath/bolso.jpeg';
+  static const String imagenReloj = '$imagePath/reloj.png';
+  static const String imagenPlayera = '$imagePath/playera.jpeg';
+  static const String imagenPantalones = '$imagePath/pantalones.jpeg';
+  static const String imagenBotella = '$imagePath/botella.jpeg';
+  static const String imagenAuriculares = '$imagePath/auriculares.jpeg';
+  static const String imagenMancuernas = '$imagePath/mancuernas.jpeg';
 }
