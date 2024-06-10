c5750fdb43571c766f5481d6f0004c8b1857ac47
JUANIMAN <juanimandarin9@gmail.com>
2024-06-09 22:24:32 -0600

    Se soluciono el problema que causaba que no se puediera hacer scroll en la vista de productos

--- lib/vistas/producto.dart
+++ lib/vistas/producto.dart
@@ -52,155 +52,162 @@ class _ProductDetailState extends State<ProductDetail> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Producto'),
       ),
-      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        if (widget.product.images.length > 1)
-          CarouselSlider(
-            items: widget.product.images.map((image) {
-              return Builder(
-                builder: (BuildContext context) {
-                  return GestureDetector(
-                    onTap: () {
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return Hero(
-                            tag: 'imageHero',
-                            child: PhotoView(
-                              imageProvider: NetworkImage(image),
-                              initialScale:
-                                  PhotoViewComputedScale.contained * 1,
-                              minScale: PhotoViewComputedScale.contained * 1,
-                              maxScale: PhotoViewComputedScale.covered,
-                            ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (widget.product.images.length > 1)
+              CarouselSlider(
+                items: widget.product.images.map((image) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return GestureDetector(
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return Hero(
+                                tag: 'imageHero',
+                                child: PhotoView(
+                                  imageProvider: NetworkImage(image),
+                                  initialScale: PhotoViewComputedScale.contained * 1,
+                                  minScale: PhotoViewComputedScale.contained * 1,
+                                  maxScale: PhotoViewComputedScale.covered,
+                                ),
+                              );
+                            },
                           );
                         },
-                      );
-                    },
-                    child: Image.network(
-                      image,
-                      width: double.infinity,
-                      fit: BoxFit.cover,
-                    ),
-                  );
-                },
-              );
-            }).toList(),
-            options: CarouselOptions(
-              height: 400,
-              viewportFraction: 1.0,
-              onPageChanged: (index, reason) {
-                setState(() {
-                  _currentIndex = index;
-                });
-              },
-            ),
-          )
-        else if (widget.product.images.isNotEmpty)
-          GestureDetector(
-            onTap: () {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return Hero(
-                    tag: 'imageHero', // Same tag as the first Hero
-                    child: PhotoView(
-                      imageProvider: NetworkImage(widget.product.images.first),
-                      initialScale: PhotoViewComputedScale.contained * 1,
-                      minScale: PhotoViewComputedScale.contained * 1,
-                      maxScale: PhotoViewComputedScale.covered,
-                    ),
-                  );
-                },
-              );
-            },
-            child: Image.network(
-              widget.product.images.first,
-              height: 400,
-              width: double.infinity,
-              fit: BoxFit.cover,
-            ),
-          ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: widget.product.images.map((url) {
-            int index = widget.product.images.indexOf(url);
-            return Container(
-              width: 8.0,
-              height: 8.0,
-              margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: _currentIndex == index
-                    ? Theme.of(context).primaryColor
-                    : const Color.fromRGBO(0, 0, 0, 0.4),
-              ),
-            );
-          }).toList(),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Text(
-            widget.product.name,
-            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Text(
-            '\$${widget.product.price.toStringAsFixed(2)}',
-            style: const TextStyle(fontSize: 20),
-          ),
-        ),
-        const SizedBox(height: 20),
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16.0),
-          child: Text(
-            "Descripción",
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Text(
-            widget.product.description,
-            style: const TextStyle(fontSize: 16),
-          ),
-        ),
-        const SizedBox(height: 20),
-        Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Visibility(
-              visible: widget.propietario,
-              child: Row(
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => EditarProducto(
-                            product: widget.product,
-                          ),
+                        child: Image.network(
+                          image,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
                         ),
                       );
                     },
-                    child: const Text('EDITAR'),
-                  ),
-                  const SizedBox(height: 10), // Espacio entre los botones
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.red,
-                        foregroundColor: Colors.white),
-                    onPressed: () async {
-                      await deleteProductImage(widget.product);
-                      await deleteProduct(widget.product);
+                  );
+                }).toList(),
+                options: CarouselOptions(
+                  height: 400,
+                  viewportFraction: 1.0,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      _currentIndex = index;
+                    });
+                  },
+                ),
+              )
+            else if (widget.product.images.isNotEmpty)
+              GestureDetector(
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return Hero(
+                        tag: 'imageHero',
+                        child: PhotoView(
+                          imageProvider: NetworkImage(widget.product.images.first),
+                          initialScale: PhotoViewComputedScale.contained * 1,
+                          minScale: PhotoViewComputedScale.contained * 1,
+                          maxScale: PhotoViewComputedScale.covered,
+                        ),
+                      );
                     },
-                    child: const Text('ELIMINAR'),
-                  ),
-                ],
+                  );
+                },
+                child: Image.network(
+                  widget.product.images.first,
+                  height: 400,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
               ),
-            )),
-      ]),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: widget.product.images.map((url) {
+                int index = widget.product.images.indexOf(url);
+                return Container(
+                  width: 8.0,
+                  height: 8.0,
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 2.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: _currentIndex == index
+                        ? Theme.of(context).primaryColor
+                        : const Color.fromRGBO(0, 0, 0, 0.4),
+                  ),
+                );
+              }).toList(),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
+                widget.product.name,
+                style:
+                    const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
+                '\$${widget.product.price.toStringAsFixed(2)}',
+                style: const TextStyle(fontSize: 20),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
+                "Descripción",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
+                widget.product.description,
+                style: const TextStyle(fontSize: 16),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Visibility(
+                visible: widget.propietario,
+                child: Row(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => EditarProducto(
+                              product: widget.product,
+                            ),
+                          ),
+                        );
+                      },
+                      child: const Text('EDITAR'),
+                    ),
+                    const SizedBox(height: 10), // Espacio entre los botones
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.red,
+                          foregroundColor: Colors.white),
+                      onPressed: () async {
+                        await deleteProductImage(widget.product);
+                        await deleteProduct(widget.product);
+                      },
+                      child: const Text('ELIMINAR'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
